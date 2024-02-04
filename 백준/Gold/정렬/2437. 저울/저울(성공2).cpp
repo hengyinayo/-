989b5e8231e597193181ee2e7008b0d3c73db76e
hengyinayo <wuheng@yonsei.ac.kr>
2024-02-04 21:40:15 +0900

    feat: 2/4 정렬

---
+++ 백준/Gold/정렬/2437. 저울/저울(성공2).cpp
@@ -0,0 +1,38 @@
+/*
+접근 방법:
+- 실패 코드에서의 해결법을 참고.
+*/
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) (v).begin(), (v).end()
+#define For(i,j,k) for (int i=(int)(j);i<=(int)(k);i++)
+#define Rep(i,j,k) for (int i=(int)(j); i>=(int)(k);i--)
+#define coutn cout << "\n"
+using namespace std;
+
+vector<int> v;
+int n;
+int sum=0;
+
+int main() {
+    cin >> n;
+    
+    For(i,0,n-1) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    sort(v.begin(), v.end());
+
+    For(i,0,n-1) {
+        if (v[i] > sum + 1) break;
+        sum += v[i];
+    }
+
+    cout << sum + 1;
+
+    return 0;
+}
