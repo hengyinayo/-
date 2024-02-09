8ce714ae7649b9456c93dba6b727798bdebe2bba
hengyinayo <wuheng@yonsei.ac.kr>
2024-02-10 02:12:36 +0900

    feat: 2/10 숫자 카드 2 (시간초과 원인 알아봐야됨)

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2(성공).cpp
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2(성공).cpp
@@ -3,6 +3,7 @@
 - 정렬:
 - 각 M개의 숫자를 for loop으로 iterate하면서 binary_search()함수를 사용하여 이진 탐색을 진행.
 - lower_bound과 upper_bound함수를 이용해서, lower_bound와 upper_bound의 차이를 계산 -> 이것이 바로 타겟 넘버의 개수이다.
+- 시간초과는 cin.tie(NULL)로 해결했다.
 */
 #include <bits/stdc++.h>
 #define ll long long
