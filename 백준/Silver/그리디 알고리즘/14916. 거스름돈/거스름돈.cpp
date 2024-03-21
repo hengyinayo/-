8b56ce70eaef0f34d59ff176d80fa6702f4dbe6b
hengyinayo <wuheng@yonsei.ac.kr>
2024-03-21 19:15:58 +0900

    feat: 3/21 그리디 알고리즘

--- 백준/Silver/그리디 알고리즘/14916. 거스름돈/거스름돈.cpp
+++ 백준/Silver/그리디 알고리즘/14916. 거스름돈/거스름돈.cpp
@@ -33,3 +33,25 @@ int main() {
     cout << cnt;
     return 0;
 }
+
+/*
+해설:
+- 
+
+int main() {
+    cin >> n;
+    int start = n / 5; // 최대한 5원짜리 사용
+    int remain = (n - start * 5) % 2;
+    while(remain != 0) { // 2원짜리로 나눴을 때 나머지가 0이면
+        if (start == 0){
+            cout << -1;
+            return 0;
+        } // 거슬러 줄 수 없는 돈 (5원짜리가 음수가 됨)
+        start -= 1; // 5원짜리 동전 한개 줄임
+        remain = (n-start*5) % 2; // 5원짜리 동전 빼고 나머지를 2원으로 나눠줌
+    }
+    cout << start + (n - start * 5) / 2; // 5원짜리 + 2원짜리 개수
+    return 0;
+}
+
+*/
