66e8c7dee84f3395ec2b93cd8d2ea5f95eeeb865
hengyinayo <wuheng@yonsei.ac.kr>
2024-03-21 20:13:03 +0900

    feat: 3/21 그리디 알고리즘

--- 백준/Silver/그리디 알고리즘/1343. 폴리오미노/폴리오미노.cpp
+++ 백준/Silver/그리디 알고리즘/1343. 폴리오미노/폴리오미노.cpp
@@ -52,3 +52,72 @@ int main() {
 
     return 0;
 }
+
+/*
+풀이 1:
+
+접근 방법:
+1. 입력 받을 보드판과 최종 결과값을 선언한다(string).
+2. '.'이 나올 때까지 순회한다.
+3. 한 글자씩 검사해, 글자가 'X'이면 카운트를 센다.
+4. '.'이 나왔을 때 카운트가 홀수이면 반복문을 빠져나와 -1을 출력한다.
+5. '.'이 나왔을 때 카운트가 짝수이면 결과값에 '.' 추가.
+6. 카운트가 2가 되면 결과값에 "BB" 추가.
+7. 카운트가 4가 되면 결과값에 "AAAA" 추가.
+8. 최종 결과 출력
+
+int cnt = 0;
+string board, output = "";
+
+int main() {
+    cin >> board;
+    For(i, 0, board.length()-1) {
+        if (board[i] == 'X') cnt++;
+
+        if (board[i] == '.') {
+            output += '.';
+
+            if (cnt % 2 != 0) break;
+            else cnt = 0;
+        }
+
+        if (cnt == 2 && board[i+1] != 'X') {
+            output += "BB";
+            cnt = 0;
+        }
+
+        if (cnt == 4) {
+            output +=  "AAAA";
+            cnt = 0;
+        }
+    }
+
+    if (cnt % 2 == 1) cout << -1;
+    else cout << output;
+
+    return 0;
+}
+*/
+
+/*
+풀이 2:
+
+접근 방법:
+- 문자열을 replace시키는 방법
+
+string str;
+
+int main() {
+    cin >> str;
+
+    str = regex_replace(str, regex("XXXX"), "AAAA");
+    str = regex_replace(str, regex("XX"), "BB");
+
+    // str.find("X"): X를 찾지 못하면 -1을 반환
+    // 여기서는 찾으면 -1을 출력해야하니, != -1 이면 -1 출력.
+    if (str.find("X") != -1) cout << -1;
+    else cout << str;
+
+    return 0;
+}
+*/
