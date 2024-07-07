30beb0fa41ec13dba3bb7f2f9f2aa582b0b581da
hengyinayo <wuheng@yonsei.ac.kr>
2024-07-07 14:08:38 +0900

    feat: 07/07 수학

--- 백준/Bronze/수학/2292. 벌집/벌집.cpp
+++ 백준/Bronze/수학/2292. 벌집/벌집.cpp
@@ -20,3 +20,18 @@ int main() {
     cout << cnt;
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cin >> number;
+    int i = 0;
+    for(int sum=2; sum <= number; i++)
+        sum += 6*i;
+    if(number == 1) i=1;
+    cout << i;
+    return 0;
+}
+*/
